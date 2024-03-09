e12ba201c1f9914360dac01664871e6673722ba4
morteza dehghani <65276280+mori-cyber@users.noreply.github.com>
2024-03-09 13:48:00 +0330

    Add files via upload

---
+++ Assignment_2-token-3/Program.cs
@@ -0,0 +1,5 @@
+﻿var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, "http://numbersapi.com/20");
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+Console.WriteLine(await response.Content.ReadAsStringAsync());
