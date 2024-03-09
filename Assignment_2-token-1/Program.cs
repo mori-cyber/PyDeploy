2372a80e5c37f6e8708163127a1e50e2fec1c266
morteza dehghani <65276280+mori-cyber@users.noreply.github.com>
2024-03-09 13:46:21 +0330

    Add files via upload

---
+++ Assignment_2-token-1/Program.cs
@@ -0,0 +1,5 @@
+﻿var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, "https://www.fruityvice.com/api/fruit/banana");
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+Console.WriteLine(await response.Content.ReadAsStringAsync());
