c6d54a95f57040db6a6c01fb654e58b918ae56d8
morteza dehghani <65276280+mori-cyber@users.noreply.github.com>
2024-03-09 13:48:57 +0330

    Add files via upload

---
+++ Assignment_2-token-4/Program.cs
@@ -0,0 +1,5 @@
+﻿var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, "https://api.dictionaryapi.dev/api/v2/entries/en/book");
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+Console.WriteLine(await response.Content.ReadAsStringAsync());
