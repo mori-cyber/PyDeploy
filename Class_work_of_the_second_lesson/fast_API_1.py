6e1ba988fceb95d9ff7cacf389c82c6d21f9a2f5
morteza dehghani <65276280+mori-cyber@users.noreply.github.com>
2024-03-14 14:16:31 +0330

    Add files via upload

---
+++ Class_work_of_the_second_lesson/fast_API_1.py
@@ -0,0 +1,17 @@
+
+#https://fastapi.tiangolo.com/
+
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+print(app)
+
+@app.get("/morteza")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
