7d709c96caacaa29bcb150d70d69f9b04e3b114c
morteza dehghani <65276280+mori-cyber@users.noreply.github.com>
2024-05-27 21:16:45 +0330

    Delete Assignment_9 directory

--- Assignment_9/Database.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from sqlmodel import SQLModel, create_engine
-
-
-@st.cache_resource
-def connect_to_database():
-    engine = create_engine("sqlite:///database.db")  # Create database
-    SQLModel.metadata.create_all(engine)  # Create tables
-    return engine
