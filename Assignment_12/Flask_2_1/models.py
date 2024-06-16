6cb182d4914f3798ce3184ad371d8c42a15245f8
morteza dehghani <65276280+mori-cyber@users.noreply.github.com>
2024-06-16 18:21:09 +0330

    Delete Assignment_12 directory

--- Assignment_12/Flask_2_1/models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from sqlmodel import Field, SQLModel
-
-
-class User(SQLModel, table=True):
-    __table_args__ = {"extend_existing":True}# از دوباره ساخته شدن جدول یا همان تیبل در هر بار اجرا جلوگیری می کند
-    id : Optional[int]=Field(default=None, primary_key=True)
-    email : str
-    password: Optional[str]
