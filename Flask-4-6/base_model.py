ac18e7f2a5f5a98b7f3633d476242258be76be31
morteza dehghani <65276280+mori-cyber@users.noreply.github.com>
2024-07-20 14:00:39 +0330

    Add files via upload

---
+++ Flask-4-6/base_model.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from sqlmodel import Field
+
+class RegisterModel(BaseModel):
+    first_name:str 
+    last_name: str
+    email :    str
+    age :      int 
+    country :  str 
+    city:      str
+    username:  str
+    password:  str
+    confirm_password: str
+      
+class LoginModel(BaseModel):
+    username: str
+    password: str
+    confirm_password: str
